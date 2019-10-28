ac77b6242e15877061978e68d9ad72669b831b81
r3gen <cjhdgs@gmail.com>
2019-10-28 02:39:58 +0000

    adding sizebot.py

---
+++ sizebot.py
@@ -0,0 +1,47 @@
+# Work with Python 3.6
+import random
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+TOKEN = ''
+
+@bot.command()
+async def sizeme(ctx):
+    msg = '{0.author.mention} is '.format(ctx.message) + get_size() + " tall."
+    await ctx.send(msg)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    
+def get_size():
+    size = {
+        1: "1 mm",
+        2: "1 cm",
+        3: "1 inch",
+        4: "3 inch",
+        5: "6 inch",
+        6: "1 foot",
+        7: "3 foot",
+        8: "6 foot",
+        9: "12 foot",
+        10: "50 foot",
+        11: "100 foot",
+        12: "200 foot",
+        13: "400 foot",
+        14: "500 foot",
+        15: "600 foot",
+        16: "800 foot",
+        17: "1000 foot",
+        18: "2000 foot",
+        19: "3000 foot",
+        20: "5000 foot"
+    }
+    return size.get(random.randint(1,20),"No size for you")
+
+bot.run(TOKEN)
