1c99a1a8d6b916769c72ee34d81cf8bf8c0e959d
r3gen <cjhdgs@gmail.com>
2020-08-01 14:58:56 -0400

    Adding Dockerfile and updates to reset

--- sizebot.py
+++ sizebot.py
@@ -16,17 +16,11 @@ def load_config():
     bot_config = ConfigParser()
     if path.exists(config_filename):
         bot_config.read(config_filename)
-        last_date = datetime.fromisoformat(bot_config["Default"]["last_reset"])
-        if last_date.date() < datetime.now().date():
-            for section in bot_config.sections():
-                if section != "Default":
-                    bot_config.remove_section(section)
-            bot_config["Default"]["last_reset"] = datetime.now().isoformat()
-            save_config(bot_config)
+        reset_config(bot_config)
     else:
         bot_config['Default'] = {
             "reset_hour": '5',
-            "last_reset": datetime.now()
+            "last_reset": datetime.now().isoformat()
         }
         save_config(bot_config)
 
@@ -38,12 +32,24 @@ def save_config(bot_config):
         bot_config.write(config_file)
 
 
+def reset_config(bot_config):
+    last_date = datetime.fromisoformat(bot_config["Default"]["last_reset"])
+    if last_date.date() < datetime.now().date():
+        for section in bot_config.sections():
+            if section != "Default":
+                bot_config.remove_section(section)
+                bot_config.add_section(section)
+        bot_config["Default"]["last_reset"] = datetime.now().isoformat()
+        save_config(bot_config)
+
+
 def read_token():
-    # import os
-    # return os.environ["SIZEBOT_TOKEN"]
-    with open("token.txt", "r") as f:
+    import os
+    return os.environ["SIZEBOT_TOKEN"]
+    '''with open("token.txt", "r") as f:
         lines = f.readlines()
         return lines[0].strip()
+    '''
 
 
 TOKEN = read_token()
@@ -55,6 +61,7 @@ config = load_config()
 
 @bot.command()
 async def sizeme(ctx):
+    reset_config(config)
     user_id = "{}".format(ctx.author.id)
     server = "{}".format(ctx.guild.id)
     if config.has_option(server, user_id):
@@ -72,12 +79,13 @@ async def sizeme(ctx):
 
 @bot.command()
 async def showsizes(ctx):
+    reset_config(config)
     server = '{}'.format(ctx.guild.id)
     msg = "All sizes:\n"
     for user in config.options(server):
         member = await ctx.guild.fetch_member(user)
-        username = member.nick
-        msg += username + ": " + config[server][user] + "\n"
+        username = member.display_name
+        msg += str(username) + ": " + config[server][user] + "\n"
     await ctx.send(msg)
 
 
